4d5bfc75b6f585dc222e87f4d98b610fd5a4922d
Hicham2012 <99765449+Hicham2012@users.noreply.github.com>
2023-11-05 13:39:31 +0100

    add static routes

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def sayHello(request):
-  return HttpResponse('Hello')
+def index(request):
+  return render(request, 'index.html', {})
