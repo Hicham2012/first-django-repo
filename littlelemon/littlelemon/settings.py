8609790fee43ed872154a2b919ac8ad43e6b92ce
Hicham2012 <99765449+Hicham2012@users.noreply.github.com>
2023-11-07 23:37:53 +0100

    connect Django app to mylemon database

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -77,8 +77,18 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # Enter MariaDB database info
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mylemon',
+        'USER':'root',
+        'PASSWORD': 'Xtox2012@',
+        'HOST':'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
